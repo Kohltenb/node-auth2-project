192070865996b23d771b2cbe7401567517f7f969
Kohlten Bills <kohltenbillsy@gmail.com>
2022-07-27 15:08:29 -0600

    complete

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -54,7 +54,7 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       token,
     })
   } else {
-    next({ status: 401, message: 'Invalid credentials'})
+    next({ status: 401, message: 'invalid credentials'})
   }
 });
 
